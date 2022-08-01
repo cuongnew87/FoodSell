5bdcc82fc0d2597365382ead72375fb3b2579a9c
cuongnew87 <cuongnew37@gmail.com>
2022-08-01 22:05:32 +0700

    add cors

--- resources/views/master.blade.php
+++ resources/views/master.blade.php
@@ -5,6 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta name="description" content="">
     <meta name="author" content="">
+    <meta http-equiv="Content-Security-Policy" content="upgrade-insecure-requests">
     <title>Home | E-Shopper</title>
     <base href="{{asset('')}}">
     <link href="source/css/bootstrap.min.css" rel="stylesheet">
